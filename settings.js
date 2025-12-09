de7dcdd2a558cb955d4b5a23fcd6fc5a51fd3bde
dossasam <newsam052@gmail.com>
2025-12-09 17:00:12 +0000

    Update settings.js for SAM-2

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "Ice~0QNpau", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "22891153473", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~0QNpau",
+  OWNER_NUMBER: "+22891153473", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
