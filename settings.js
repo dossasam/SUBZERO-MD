312d049c60b570b4fce826b005401b44a41ba484
dossasam <newsam052@gmail.com>
2025-12-09 16:30:10 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "Ice~iV0Ci3", // Put Session Id Here Inside "*"
+  SESSION_ID: "Ice~0QNpau", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "22891153473", // Put Owner Number Here 
-  PREFIX: "*", // Default Prefix You Can Change To .,*!?+-&%#@
+  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
